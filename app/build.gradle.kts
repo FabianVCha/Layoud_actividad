f9fc558d164d6bc84fc8f8eae4ebe7ac09e34d37
Erick Chaparro <125765020+3lPr1mo@users.noreply.github.com>
2024-03-15 22:36:09 -0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "Sapo.layoud_actividad"
-    compileSdk = 33
+    compileSdkVersion(34)
 
     defaultConfig {
         applicationId = "Sapo.layoud_actividad"
-        minSdk = 29
-        targetSdk = 33
+        targetSdkVersion(34)
+        minSdkVersion(29)
         versionCode = 1
         versionName = "1.0"
 
@@ -51,6 +51,11 @@ android {
 
 dependencies {
 
+    val nav_version = "2.5.3"
+    // Java language
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
@@ -60,6 +65,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +73,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
--- app/src/main/java/Sapo/layoud_actividad/Activity_main.java
+++ app/src/main/java/Sapo/layoud_actividad/Activity_main.java
@@ -1,8 +1,13 @@
 package Sapo.layoud_actividad;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -12,6 +17,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -20,121 +26,13 @@ import android.widget.Toast;
 
 public class Activity_main extends AppCompatActivity implements View.OnClickListener{
 
-  //VARIABLES QUE SE VAN A USAR
-  private TextView TITULO_C;
-  private TextView SALIDA_T;
-  private EditText NOMBRE_C;
-  private EditText BUSQUEDA_C;
-  private Button BUSCAR_C;
-  private RadioGroup SELECCION_C;
-  private Spinner DISPONIBLE_C;
-
-
-
-// codigo de funciones asignadas
+  @SuppressLint("MissingInflatedId")
   @Override
   protected void onCreate(Bundle savedInstanceStare) {
 
-
     super.onCreate(savedInstanceStare);
-    setContentView(R.layout.linear);//se relaciona con el layoud ya creado
-
-    //TEXTVIEW
-    SALIDA_T = (TextView) findViewById(R.id.MENSAJE);
-    TITULO_C = (TextView) findViewById(R.id.TITULO); //relaciona TITULO_C con el textview del layoud
-    TITULO_C.setTextSize(40);
-    TITULO_C.setTextColor(Color.BLUE);
-    TITULO_C.setText("HOTELES FACUNDO"); //este es el texto que se vera en pantalla
-
-    //EDITTEXT
-    NOMBRE_C = (EditText) findViewById(R.id.NOMBRE);
-    NOMBRE_C.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-      }
-
-      @Override
-      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-      }
-
-      @Override
-      public void afterTextChanged(Editable editable) {
-        NOMBRE_C.setTextColor(Color.GREEN);
-
-      }
-    });
-
-    BUSQUEDA_C = (EditText) findViewById(R.id.BUSQUEDA);
-    BUSQUEDA_C.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-      }
-
-      @Override
-      public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-      }
-
-      @Override
-      public void afterTextChanged(Editable editable) {
-        BUSQUEDA_C.setTextColor(Color.GREEN);
-      }
-    });//LISTENER PARA EL CAMBIO DE CONTENIDO DE VARIABLE
-
-    //BUTTON
-    BUSCAR_C = (Button) findViewById(R.id.BUSCAR);
-    BUSCAR_C.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        SALIDA_T.setText("ANFRITION " + NOMBRE_C.getText() + "SI HAY DISPONIVILIDAD PARA " + BUSQUEDA_C.getText());
-      }
-    });
-
-    //RADIOGROUP
-
-    SELECCION_C = (RadioGroup) findViewById(R.id.SELECCION);
-    SELECCION_C.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-      @Override
-      public void onCheckedChanged(RadioGroup radioGroup, int i) {
-        if (i == R.id.PISCINA) {
-
-        } else if (i == R.id.SAUNA) {
-
-        }
-      }
-    });
-
-
-    //Spinner
-
-    ArrayAdapter<CharSequence> adapter  = ArrayAdapter.createFromResource(
-      this,
-      R.array.fechas,
-      android.R.layout.simple_spinner_item
-    );
-
-    DISPONIBLE_C = (Spinner) findViewById(R.id.DIPONIBLE);
-    DISPONIBLE_C.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-      @Override
-      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String fecha = parent.getItemAtPosition(position).toString();
-        Toast.makeText(getBaseContext(), "seleccionado"+fecha, Toast.LENGTH_LONG).show();
-
-      }
-
-      @Override
-      public void onNothingSelected(AdapterView<?> adapterView) {
-
-      }
-    });
-
-
-
+    setContentView(R.layout.primer);//se relaciona con el layoud ya creado
   }
-
-
   @Override
   public void onClick(View view) {
 
